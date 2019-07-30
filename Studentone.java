d65619acdd430d1a8d9d6f7b2164ee2bb18f665f
swami3690 <53114582+swami3690@users.noreply.github.com>
2019-07-30 22:45:28 +0100

    Add files via upload

---
+++ Studentone.java
@@ -0,0 +1,24 @@
+public class Studentone {
+    public static void main (String args []){
+        String Name = "Arpi";
+        System.out.println(Name);
+        String School  = "Swaminarayan";
+        System.out.println (School);
+        int stundentno = 9;
+        System.out.println(stundentno);
+        double schooltime = (9.45);
+        System.out.println(schooltime);
+        float lunchtime = (2.39F);
+        System.out.println(lunchtime);
+
+
+
+
+
+
+
+
+    }
+
+
+}
